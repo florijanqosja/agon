3dfa72ba6498d19e9932bad1c4a74f32112ca050
florijanqosja <florijanqosja@gmai.com>
2021-07-05 01:08:30 +0100

    init

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log("jell")
+aa = console.log("jell")
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import aa from "./components/header"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <aa />
   </React.StrictMode>,
   document.getElementById('root')
 );
